16de100f153158dddcbf437257d498e950feedd3
phaer <hello@phaer.org>
2024-07-18 12:26:04 +0200

    add version to 500-most-popular-pypi-packages

--- scripts/get-500-most-popular-pypi-packages.sh
+++ scripts/get-500-most-popular-pypi-packages.sh
@@ -1,6 +1,12 @@
 # Source: https://hugovk.github.io/top-pypi-packages/
-
-curl https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.min.json \
+set -ex
+pkg_names="$(curl -s https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.min.json \
     | jq -r '.rows[:500][] | .project' \
-    | sort \
-    > 500-most-popular-pypi-packages.txt
+    | sort)";
+for pkg_name in $pkg_names
+do
+    pkg_version="$(curl -s "https://pypi.org/pypi/$pkg_name/json" \
+        | jq -r .info.version \
+        )"
+    echo "$pkg_name==$pkg_version"
+done > 500-most-popular-pypi-packages.txt
